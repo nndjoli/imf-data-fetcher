a0c5f2071dfa96a72038ee9ed607e529fa1027d7
If-ai111 <116048276+If-ai111@users.noreply.github.com>
2025-06-08 19:45:55 +0200

    Remove /imfdatapy

--- imfdatapy/consts.py
+++
@@ -1,5 +0,0 @@
-# Base URL for the IMF SDMX 3.0 API endpoint
-BASE = "https://api.imf.org/external/sdmx/3.0"
-
-# HTTP headers for API requests, specifying that the response should be in JSON format
-HEADERS = {"Accept": "application/json"}
